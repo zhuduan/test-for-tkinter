9bdff3a00ea4a6778ae2571329021f54e4a82460
Haifeng Zhu <Haifeng.Zhu@activenetwork.com>
2018-10-23 17:21:20 +0800

    add initial files

---
+++ Application.py
@@ -0,0 +1,77 @@
+import tkinter
+from tkinter import ttk
+from ToolTip import ToolTip
+from ToolTip import createToolTip
+
+class Application(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        ttk.Frame.__init__(self, parent, *args, **kwargs)
+        self.root = parent
+        self.init_gui()
+
+    def on_quit(self):
+        # do anything needed 
+        quit()
+
+    def add_two_num(self):
+        num1 = int(self.num1_entry.get())
+        num2 = int(self.num2_entry.get())
+        num3 = num1 + num2
+        self.answer_label['text'] = num3
+
+    def init_gui(self):
+        self.root.title('两数相加')
+        self.root.option_add('*tearOff', 'FALSE')
+
+        self.grid(column=0, row=0, sticky='nsew')
+
+        self.menubar = tkinter.Menu(self.root)
+
+        self.menu_file = tkinter.Menu(self.menubar)
+        self.menu_file.add_command(label='退出', command=self.on_quit)
+
+        self.menu_edit = tkinter.Menu(self.menubar)
+
+        self.menubar.add_cascade(menu=self.menu_file, label='文件')
+        self.menubar.add_cascade(menu=self.menu_edit, label='退出')
+
+        self.root.config(menu=self.menubar)
+
+        self.num1_entry = ttk.Entry(self, width=5)
+        self.num1_entry.grid(column=1, row = 2)
+
+        self.num2_entry = ttk.Entry(self, width=5)
+        self.num2_entry.grid(column=3, row=2)
+
+
+        self.calc_button =ttk.Button(self, text='计算',command=self.add_two_num)
+        self.calc_button.grid(column=0, row=3, columnspan=4)
+        createToolTip(self.calc_button, "it is a button")
+
+        self.answer_frame = ttk.LabelFrame(self, text='结果',
+                height=100)
+        self.answer_frame.grid(column=0, row=4, columnspan=4, sticky='nesw')
+
+        self.answer_label = ttk.Label(self.answer_frame, text='')
+        self.answer_label.grid(column=0, row=0)
+        createToolTip(self.answer_label, "it is a label")
+
+
+        ttk.Label(self, text='两个数').grid(column=0, row=0,
+                columnspan=4)
+        ttk.Label(self, text='第一个数').grid(column=0, row=2,
+                sticky='w')
+        ttk.Label(self, text='第二个数').grid(column=2, row=2,
+                sticky='w')
+
+        ttk.Separator(self, orient='horizontal').grid(column=0,
+                row=1, columnspan=4, sticky='ew')
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+if __name__ == '__main__':
+    mainFrame = tkinter.Tk()
+
+    Application(mainFrame)
+    mainFrame.mainloop()
